fa66a79fc4e6e6a5ebb65629d77825e7cdc39c6d
Matěj Beran <matej.beran96@gmail.com>
2019-08-11 17:19:57 +0200

    First game prototype.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include "ctime"
+
+void PrintIntro(int Difficulty)
+{
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << "secure server room...\nEnter the correct code to continue...\n\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+    PrintIntro(Difficulty);
+
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << "\n+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    return CodeSum == GuessSum && GuessProduct == CodeProduct;
+}
 
 int main()
 {
-    std::cout << "You are a secret agent breaking into the server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
+    srand(time(NULL));
+
+    const int MaximumLevelDifficulty = 5;
+    int LevelDifficulty = 1;
+
+    bool bLevelComplete;
+    while (LevelDifficulty <= MaximumLevelDifficulty)
+    {
+        bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();  // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+
+    std::cout << "\n\n*** Great work! Now get out of there! ***\n\n";
 
     return 0;
 }
